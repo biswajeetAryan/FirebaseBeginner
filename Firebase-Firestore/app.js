3d172044b40fccefe3b281a0181950f9ec63d513
BISWAJEET CHATTERJEE <chatterjeebiswajeet.11@gmail.com>
2020-10-07 22:52:41 +0530

    initial Project added

---
+++ Firebase-Firestore/app.js
@@ -0,0 +1,25 @@
+const cafeList = document.querySelector('#cafe-list');
+
+function renderCafe(item){
+    let li = document.createElement('li');
+    let name = document.createElement('span');
+    let city = document.createElement('span');
+
+    li.setAttribute('data-id', item.id);
+
+    name.textContent = item.data().name;
+    city.textContent = item.data().city;
+
+    li.appendChild(name);
+    li.appendChild(city);
+
+    cafeList.appendChild(li);
+}
+
+db.collection('cafes').get().then((snapshot) => {
+    snapshot.docs.forEach(element => {
+        renderCafe(element);
+        console.log(element.data());
+    });
+    
+})
